31544679ebd98750a9dab5021dae88b099d42238
Tarcisio Rocha <tarcisio@dcomp.ufs.br>
2017-03-10 00:35:31 +0000

    Alteracao de porta

--- src/AppUDP2.java
+++ src/AppUDP2.java
@@ -4,7 +4,7 @@ import java.net.SocketException;
 public class AppUDP2 {
 
     public static void main(String[] args) throws SocketException {
-	    DatagramSocket socket = new DatagramSocket(10000);
+	    DatagramSocket socket = new DatagramSocket(20000);
 	    System.out.println("Hello World!");
     }
 }
