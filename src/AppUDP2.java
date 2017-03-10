da1b045de84e854817f0340c89fe4b31511f476d
Tarcisio Rocha <tarcisio@dcomp.ufs.br>
2017-03-10 00:24:52 +0000

    Second commit

---
+++ src/AppUDP2.java
@@ -0,0 +1,10 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class AppUDP2 {
+
+    public static void main(String[] args) throws SocketException {
+	    DatagramSocket socket = new DatagramSocket(10000);
+	    System.out.println("Hello World!");
+    }
+}
